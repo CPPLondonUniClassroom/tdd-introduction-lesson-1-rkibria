8f6683986b419a0fce5105d9be3cd1d836c33439
rkibria <rkibria@users.noreply.github.com>
2019-06-11 20:56:57 +0100

    construction test

--- test/testmain.cpp
+++ test/testmain.cpp
@@ -1,2 +1,14 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+
+#include "StringReverse.h"
+
+using namespace CPPLondonUni;
+
+TEST_CASE( "construct from string", "[construction]" ) {
+	std::string name = "abc";
+	StringReverse nameRev{name};
+
+	// REQUIRE();
+
+}
